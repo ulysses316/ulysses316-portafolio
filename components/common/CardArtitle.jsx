f2b8d2a471ead9ff87ac5493f53db4b7a681258d
ulysses316 <ulises316@live.com.mx>
2023-07-15 12:14:06 -0600

    [ADD] Card Article

---
+++ components/common/CardArtitle.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HeartIcon } from '@heroicons/react/24/outline'
+
+export default function CardArtitle() {
+    return (
+        <a href="#">
+            <div className='rounded-lg shadow-3xl shadow-2xl py-4 px-6 text-black dark:text-white'>
+                <h2 className='text-primary-red dark:text-primary-purple font-bold text-[23px]'>The CSS mindset</h2>
+                <p className='my-[16px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero cum culpa libero minus nesciunt, incidunt nostrum beatae, odio possimus dicta voluptatem fuga vitae sint provident? Quos praesentium expedita perferendis adipisci!</p>
+                <div>
+                    <hr />
+                    <div className='flex items-center gap-2 mt-3'>
+                        <HeartIcon className='w-6' />
+                        <span>505</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
+import CardArtitle from '@/components/common/CardArtitle'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -11,6 +13,15 @@ export default function Home() {
     <>
     <Navbar/>
     <Hero/>
+    
+
+    <div className='container mx-auto grid grid-cols-3 gap-6 my-12'>
+      <CardArtitle/>
+      <CardArtitle/>
+      <CardArtitle/>
+    </div>
+
+
     <Footer/>
     </>
   )
