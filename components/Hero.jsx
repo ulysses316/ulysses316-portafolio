f6c7e4369feb3cf432e97e9c02531f495fa806f1
ulysses316 <ulises316@live.com.mx>
2023-07-16 23:11:21 -0600

    [FIX] class > className on Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,26 +22,26 @@ export default function Hero() {
             <h2 className='text-[30px] sm:text-[50px] text-center lg:text-left'>{idiom.hero.subtitle}</h2>
             <p className='my-[16px] text-center lg:text-left'>{idiom.hero.text}</p>
             <div className='flex gap-2 justify-center lg:justify-normal flex-wrap lg:w-[80%] '>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#ffdf76] ring-1 ring-inset ring-[#ffdf76]/10">Python</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#7CC8D2] ring-1 ring-inset ring-[#7CC8D2]/10">Flask</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#44B78B] ring-1 ring-inset ring-[#44B78B]/20">Django</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">PostgreSQL</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">API REST</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black dark:text-white ring-1 ring-inset ring-black/20 dark:ring-white/20">GIT</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Linux</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#E44D26] ring-1 ring-inset ring-[#E44D26]/10">HTML</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#016BC1] ring-1 ring-inset ring-[#016BC1]/10">CSS</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#F7E018] ring-1 ring-inset ring-[#F7E018]/10">Javascript</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#149ECA] ring-1 ring-inset ring-[#149ECA]/20">React</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black dark:text-[#EDEDED] ring-1 ring-inset ring-black/20 dark:ring-[#EDEDED]/20">NextJS</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#F7E018] ring-1 ring-inset ring-[#F7E018]/10">ES6</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#79589F] ring-1 ring-inset ring-[#79589F]/10">Heroku</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#BF4080] ring-1 ring-inset ring-[#BF4080]/10">SASS</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">CSS Frameworks</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#CC3534] ring-1 ring-inset ring-[#CC3534]/10">NPM</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#2188B6] ring-1 ring-inset ring-[#2188B6]/10">Yarn</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#00ED64] ring-1 ring-inset ring-[#00ED64]/10">Mongo</span>
-              <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#f90] ring-1 ring-inset ring-[#f90]/10">AWS</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#ffdf76] ring-1 ring-inset ring-[#ffdf76]/10">Python</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#7CC8D2] ring-1 ring-inset ring-[#7CC8D2]/10">Flask</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#44B78B] ring-1 ring-inset ring-[#44B78B]/20">Django</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">PostgreSQL</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">API REST</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black dark:text-white ring-1 ring-inset ring-black/20 dark:ring-white/20">GIT</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Linux</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#E44D26] ring-1 ring-inset ring-[#E44D26]/10">HTML</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#016BC1] ring-1 ring-inset ring-[#016BC1]/10">CSS</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#F7E018] ring-1 ring-inset ring-[#F7E018]/10">Javascript</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#149ECA] ring-1 ring-inset ring-[#149ECA]/20">React</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black dark:text-[#EDEDED] ring-1 ring-inset ring-black/20 dark:ring-[#EDEDED]/20">NextJS</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#F7E018] ring-1 ring-inset ring-[#F7E018]/10">ES6</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#79589F] ring-1 ring-inset ring-[#79589F]/10">Heroku</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#BF4080] ring-1 ring-inset ring-[#BF4080]/10">SASS</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">CSS Frameworks</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#CC3534] ring-1 ring-inset ring-[#CC3534]/10">NPM</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#2188B6] ring-1 ring-inset ring-[#2188B6]/10">Yarn</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#00ED64] ring-1 ring-inset ring-[#00ED64]/10">Mongo</span>
+              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-[#f90] ring-1 ring-inset ring-[#f90]/10">AWS</span>
             </div>
           </div>
         </div>
