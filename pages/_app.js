4f51a5fe88db517e5112a06f917d19e3e084baaa
ulysses316 <ulises316@live.com.mx>
2023-07-18 01:31:16 -0600

    [ADD] envar to vercel

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }) {
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
+      
 
       <Script id="google-analytics" strategy="lazyOnload">
         {`
