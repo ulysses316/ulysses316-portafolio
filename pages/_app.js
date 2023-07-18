cd45e53e52054e1ad417ade2c1e2a5bc6218c302
ulysses316 <ulises316@live.com.mx>
2023-07-18 01:00:01 -0600

    [ADD] GTM

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,32 @@ import '@/styles/globals.css'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { LanguageProvider } from '@/components/context/LanguageContext'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute='class'>
-      <LanguageProvider>
-        <Component {...pageProps} />
-      </LanguageProvider>
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <ThemeProvider attribute='class'>
+        <LanguageProvider>
+          <Component {...pageProps} />
+        </LanguageProvider>
+      </ThemeProvider>
+    </>
   )
 }
