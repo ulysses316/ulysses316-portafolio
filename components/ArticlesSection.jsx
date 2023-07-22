e887801f4641e2e53d60f16cd4c8831f492d05ba
ulysses316 <ulises316@live.com.mx>
2023-07-22 01:25:27 -0600

    [MOD] estilos de los articulos

--- components/ArticlesSection.jsx
+++ components/ArticlesSection.jsx
@@ -22,6 +22,7 @@ export default function ArticlesSection() {
     }
   }, [articles]);
 
+  
   return (
     <section>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-6 gap-10 my-12'>
